83aa85270be4981a780d9420638bc6676d936b49
Artem Zaborskiy <compartia@gmail.com>
2018-07-29 23:17:45 +0300

    Heroku (#21)
    * Postinstall script 📜 🛠 
    
    * fix API url

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,3 @@
-export const REGISTRY_URL = 'http://localhost:5000';
+// export const REGISTRY_URL = 'http://localhost:5000';
+// export const REGISTRY_URL = 'https://npm-viz.herokuapp.com';
+export const REGISTRY_URL = '';
